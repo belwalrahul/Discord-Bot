560fad679c28b5b1dd53357e9a10a150cbb4b43f
Rahul Belwal <belwalrahul@gmail.com>
2021-04-30 10:19:10 +0530

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -2,7 +2,7 @@ import re
 import os
 import time
 import discord
-import youtube_dl
+from youtube_dl import YoutubeDL
 from discord.ext import commands
 from urllib.parse import urlencode
 from urllib.request import urlopen
@@ -12,6 +12,57 @@ client = commands.Bot(command_prefix = ".")
 @client.event
 async def on_ready():
     print("Ready and listening as {0.user}".format(client))
+
+@client.command()
+async def join(ctx):
+    await ctx.message.author.voice.channel.connect()
+
+@client.command()
+async def play(ctx, uri:str):
+    if ctx.author.voice:
+        await ctx.message.author.voice.channel.connect()
+
+        ydl_opts = {'format': 'bestaudio'}
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(uri, download = False)
+            url = info['formats'][0]['url']
+
+        voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+        voice.play(discord.FFmpegPCMAudio(url))
+
+    else:
+        await ctx.send("{0}, you must be connected to a voice channel!".format(ctx.message.author.name))
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Music is paused or either not playing!")
+
+@client.resume()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("Music is already playing!")
+
+@client.command()
+async def stop(ctx):
+    discord.utils.get(client.voice_clients, guild = ctx.guild).stop()
+
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+    if voice:
+        await voice.disconnect()
+    else:
+        await ctx.send("I'm not connected to any voice channel")
 
 @client.command()
 async def yt(ctx, *, search_string):
